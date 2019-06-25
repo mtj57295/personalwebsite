97f26aea206127dc1fc941e192e367b30140b44b
OEM Configuration (temporary user) <mtj57295@gmail.com>
2019-06-25 15:53:27 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
+import { BrowserRouter as Router } from ‘react-router-dom’;
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Router exact path='/' component{App}>
+  </Router>, document.getElementById(‘root’));
